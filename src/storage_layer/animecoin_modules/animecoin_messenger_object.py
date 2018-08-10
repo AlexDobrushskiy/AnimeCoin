8fa777fe50f7653789881606004cd890390a34e3
synapse <synapse@buffered.com>
2018-08-10 15:50:37 +0200

    Refactored the serializer code to use json

--- src/storage_layer/animecoin_modules/animecoin_messenger_object.py
+++ src/storage_layer/animecoin_modules/animecoin_messenger_object.py
@@ -3,6 +3,7 @@ import sys
 import base64
 import random
 import time
+import json
 
 from animecoin_modules.animecoin_signatures import animecoin_id_write_signature_on_data_func, \
     animecoin_id_verify_signature_with_public_key_func
@@ -11,7 +12,7 @@ from animecoin_modules.helpers import sleep_rand, get_sha3_512_func
 from animecoin_modules.animecoin_compression import compress_data_with_zstd_func, decompress_data_with_zstd_func
 
 
-MESSAGE_FORMAT_VERSION = '1.00'
+MESSAGE_FORMAT_VERSION = "1"
 MIN_NONCE_LENGTH = 500
 MAX_NONCE_LENGTH = 1200
 MAX_MESSAGE_SIZE = 1000
@@ -21,114 +22,48 @@ NONCE_LENGTH = random.randint(MIN_NONCE_LENGTH, MAX_NONCE_LENGTH)
 def verify_raw_message_file(raw_message_contents):
     if isinstance(raw_message_contents, str):
         if len(raw_message_contents) < 4000:
-            signature_string = '\n\ndigital_signature: \n'
-            start_string = 'START_OF_MESSAGE'
-            end_string = 'END_OF_MESSAGE'
+            hash_of_combined_message_string = get_sha3_512_func(raw_message_contents)
+
+            msg = json.loads(raw_message_contents)
+            VALID_KEYS = {"sender_id", "receiver_id", "timestamp", "message_format_version",
+                          "message_body", "random_nonce", "digital_signature"}
+
+            a, b = set(msg.keys()), VALID_KEYS
+            if len(a-b) + len(b-a) > 0:
+                raise KeyError("Keys don't match")
+
             id_character_set = 'ABCDEF1234567890'
             nonce_character_set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-            version_character_set = '1234567890.'
 
-            # start with \n
-            assert (raw_message_contents[0] == '\n')
+            senders_animecoin_id = msg["sender_id"]
+            assert (len(senders_animecoin_id) == 132)
+            assert ([(x in id_character_set) for x in senders_animecoin_id])
 
-            # start with '\n______________________\n\nSTART_OF_MESSAGE'
-            assert (raw_message_contents.split(start_string)[0] == '\n______________________\n\n')
+            receivers_animecoin_id = msg["receiver_id"]
+            assert (len(receivers_animecoin_id) == 132)
+            assert ([(x in id_character_set) for x in receivers_animecoin_id])
+            assert (senders_animecoin_id != receivers_animecoin_id)
 
-            # has exactly 42 \n
-            print("DBG: Number of \n: %s" % len(raw_message_contents.split('\n')))
-            assert (len(raw_message_contents.split('\n')) == 42)
+            timestamp_of_message = float(msg["timestamp"])
+            assert (timestamp_of_message > time.time() - 60)
+            assert (timestamp_of_message < time.time() + 60)
 
-            # contains '\n\ndigital_signature: \n' exactly once
-            assert (len(raw_message_contents.split(signature_string)) == 2)
+            message_format_version = int(msg["message_format_version"])
 
-            # message_contents is everything before '\n\ndigital_signature: \n'
-            message_contents = raw_message_contents.split(signature_string)[0]
+            message_body = msg["message_body"]
 
-            # message_contents has: END_OF_MESSAGE\n\n______________________\n'
-            assert (message_contents.split(end_string)[-1] == '\n\n______________________\n')
+            random_nonce = msg["random_nonce"]
+            assert (len(random_nonce) >= 500)
+            assert (len(random_nonce) <= 2500)
+            assert ([(x in nonce_character_set) for x in random_nonce])
 
-            # message_contents replaces all \n with ''
-            # message_contents ends with '______________________'
-            assert (message_contents.replace('\n', '')[-22:] == '______________________')
-
-            # hashes message_contents
-            hash_of_combined_message_string = get_sha3_512_func(message_contents)
-
-            # signature_line everything after '\n\ndigital_signature: \n', \n replaced
-            signature_line = raw_message_contents.split(signature_string)[-1].replace('\n', '')
-            # signature is str, 264 long, contains ABCDEF1234567890 characters
-            assert (isinstance(signature_line, str))
-            assert (len(signature_line) == 264)
-            assert ([(x in id_character_set) for x in signature_line])
-
-            # split message_contents by fields
-            message_contents_fields = message_contents.split('______________________')
-
-            # there has to be 11 fields
-            assert (len(message_contents_fields) == 11)
-
-            # ignore first and last field (which are \n)
-            for current_field in message_contents_fields[1:-1]:
-                sender_string = '\n\nsender_id:\n'
-                receiver_string = '\n\nreceiver_id:\n'
-                timestamp_string = '\n\ntimestamp:\n'
-                message_format_version_string = '\n\nmessage_format_version:\n'
-                message_size_string = '\n\nmessage_size:\n'
-                message_body_string = '\n\nmessage_body:\n'
-                random_nonce_string = '\n\nrandom_nonce:\n'
-
-                print("DBG2", current_field)
-                assert (len(message_contents.split(sender_string)) == 2)
-                assert (len(message_contents.split(receiver_string)) == 2)
-                assert (len(message_contents.split(timestamp_string)) == 2)
-                assert (len(message_contents.split(message_format_version_string)) == 2)
-                assert (len(message_contents.split(message_size_string)) == 2)
-                assert (len(message_contents.split(message_body_string)) == 2)
-                assert (len(message_contents.split(random_nonce_string)) == 2)
-
-                if current_field[:len(sender_string)] == sender_string:
-                    senders_animecoin_id = current_field.replace(sender_string, '').replace('\n', '')
-                    assert (len(senders_animecoin_id) == 132)
-                    assert ([(x in id_character_set) for x in senders_animecoin_id])
-
-                if current_field[:len(receiver_string)] == receiver_string:
-                    receivers_animecoin_id = current_field.replace(receiver_string, '').replace('\n', '')
-                    assert (len(receivers_animecoin_id) == 132)
-                    assert ([(x in id_character_set) for x in receivers_animecoin_id])
-                    assert (senders_animecoin_id != receivers_animecoin_id)
-
-                if current_field[:len(timestamp_string)] == timestamp_string:
-                    timestamp_of_message = float(current_field.replace(timestamp_string, '').replace('\n', ''))
-                    assert (timestamp_of_message > time.time() - 60)
-                    assert (timestamp_of_message < time.time() + 60)
-
-                if current_field[:len(message_format_version_string)] == message_format_version_string:
-                    message_format_version = current_field.replace(message_format_version_string, '').replace('\n', '')
-                    assert (len(message_format_version) < 6)
-                    assert ([(x in version_character_set) for x in message_format_version])
-                    assert ('.' in message_format_version)
-
-                if current_field[:len(message_size_string)] == message_size_string:
-                    message_size = int(current_field.replace(message_size_string, '').replace('\n', ''))
-                    assert (message_size >= 10)
-                    assert (message_size <= 1000)
-
-                if current_field[:len(message_body_string)] == message_body_string:
-                    message_body = current_field.replace(message_body_string, '').replace('\n', '')
-                    print("DBG message_body len != message_size -> %s != %s" % (len(message_body), message_size))
-                    assert (len(message_body) == message_size)
-                    assert (message_body == message_body.encode('utf-8', 'strict').decode('utf-8', 'strict'))
-
-                if current_field[:len(random_nonce_string)] == random_nonce_string:
-                    random_nonce = current_field.replace(random_nonce_string, '').replace('\n', '')
-                    assert (len(random_nonce) >= 500)
-                    assert (len(random_nonce) <= 2500)
-                    assert ([(x in nonce_character_set) for x in random_nonce])
-                sleep_rand()
+            sleep_rand()
+            signature_line = msg["digital_signature"]
             verified = animecoin_id_verify_signature_with_public_key_func(hash_of_combined_message_string,
                                                                           signature_line, senders_animecoin_id)
             sleep_rand()
-            return verified, senders_animecoin_id, receivers_animecoin_id, timestamp_of_message, message_size, message_body, random_nonce, message_contents, signature_line
+
+            return verified, senders_animecoin_id, receivers_animecoin_id, timestamp_of_message, len(message_body), message_body, random_nonce, signature_line
 
 
 def verify_compressed_message_file(senders_animecoin_id, compressed_binary_data,
@@ -179,38 +114,37 @@ def generate_message_func(privkey, pubkey, target_pubkey, message_body):
         print('Nonce Length:' + str(len(random_nonce)))
         message_size = len(message_body)
         if (message_size - NONCE_LENGTH) < MAX_MESSAGE_SIZE:
-            message_size = message_size
             message_body = message_body
         else:
             raise ValueError("Error, message body is too large!")
     else:
         raise TypeError("Message body is not str!")
 
-    linebreak = '\n______________________\n'
+    msg = {
+        "sender_id":               pubkey,
+        "receiver_id":             target_pubkey,
+        "timestamp":               str(timestamp),
+        "message_format_version":  message_format_version,
+        "message_body":            message_body,
+        "random_nonce":            random_nonce,
+        "digital_signature":       None,
+    }
 
-    message_body = '\ntimestamp:\n' + linebreak
-    message_size = len(message_body)
+    # TODO: use HMAC for signing!
 
-    combined_message = linebreak + '\nSTART_OF_MESSAGE:\n'
-    combined_message += linebreak + '\nsender_id:\n' + pubkey + linebreak
-    combined_message += '\nreceiver_id:\n' + target_pubkey + linebreak
-    # combined_message += '\ntimestamp:\n' + str(timestamp) + linebreak
-    combined_message += '\nmessage_format_version:\n' + message_format_version + linebreak
-    combined_message += '\nmessage_size:\n' + str(10) + linebreak
-    combined_message += '\nmessage_body:\n' + message_body
-    combined_message += '\nrandom_nonce:\n' + random_nonce + linebreak + linebreak
-    combined_message += '\nEND_OF_MESSAGE\n' + linebreak
-    hash_of_combined_message_string = get_sha3_512_func(combined_message)
+    msg_serialized = json.dumps(msg)
+    hash_of_combined_message_string = get_sha3_512_func(msg_serialized)
 
     # sign and append signature to the combined message
     signature_on_raw_message = animecoin_id_write_signature_on_data_func(hash_of_combined_message_string, privkey, pubkey)
-    signed_combined_message = combined_message + '\n\ndigital_signature: \n' + signature_on_raw_message
+    msg["digital_signature"] = signature_on_raw_message
+    msg_serialized_with_signature = json.dumps(msg)
 
     # compress and generate signature
-    compressed_signed_combined_message = compress_data_with_zstd_func(signed_combined_message)
+    compressed_signed_combined_message = compress_data_with_zstd_func(msg_serialized_with_signature)
     hash_of_compressed_signed_combined_message = get_sha3_512_func(compressed_signed_combined_message)
     signature_on_compressed_message = animecoin_id_write_signature_on_data_func(hash_of_compressed_signed_combined_message, privkey, pubkey)
 
     sleep_rand()
 
-    return signed_combined_message, compressed_signed_combined_message, signature_on_compressed_message
+    return msg_serialized_with_signature, compressed_signed_combined_message, signature_on_compressed_message
