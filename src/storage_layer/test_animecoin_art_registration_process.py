f41d6a7bc995f109292b5ba2e9188f9b0b3bb9bf
synapse <synapse@buffered.com>
2018-08-24 17:55:37 +0200

    Added new style models

--- src/storage_layer/test_animecoin_art_registration_process.py
+++ src/storage_layer/test_animecoin_art_registration_process.py
@@ -1,53 +1,75 @@
 import sys
 import base64
 import zstd
+import keras
+import pickle
 
 # TODO: do we want to use sha3_512 for this?
+from dht_prototype.masternode_modules.animecoin_modules.animecoin_dupe_detection import DupeDetector,\
+    combine_fingerprint_vectors
 from dht_prototype.masternode_modules.animecoin_modules.helpers import get_sha3_512_func_bytes
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_signatures import animecoin_id_write_signature_on_data_func, animecoin_id_verify_signature_with_public_key_func
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_keys import animecoin_id_keypair_generation_func
-from dht_prototype.masternode_modules.models import ArtRegistrationRequest, load_artregistration_ticket,\
-    serialize_artregistration_ticket, serialize_signatures, load_signatures
+from dht_prototype.masternode_modules.models import ArtRegistrationRequest, load_ticket,\
+    serialize_ticket, serialize_signatures, load_signatures, ArtActivationTicket
 from dht_prototype.masternode_modules.model_validators import ARTREGISTRATION_KEYS
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_compression import compress, decompress
+
+
+# TODO: This was set by Jeff, do NOT change yet!
+TARGET_SIZE = (224, 224)
+
 
 # step 1
 # X artist constructs reg ticket
 # X sign ticket
 # X for every MN:
-#   X MN validates signature
-#   X MN validates hash
+#   o validate registration ticket
+#     X MN validates signature
+#     X MN validates hash
+#   o blockchain validations
+#     o is a unique registration (does not exist yet on the chain)
 #   X MN validates ticket
 #   X if all is well, MN signs
 # o make sure validator works right
-#   o validate thumbnail
 #   o validate metadata
-#   o validate
+#     o validate thumbnail
 # X blockchain verifies ticket with all signatures
 
 # step 2
-# o artist constructs verification ticket
-# o sign ticket
+# X artist constructs verification ticket
+# X sign ticket
 # o for every MN:
-#   o validate signature
-#   o validate hash
-#   o validate that activation ticket belongs to registration ticket
-#   o validate that registration ticket has not been activated yet
-#   o validate that image is the one in registration ticket
-#   o generate and verify fingerprints from registration ticket
-#   o generate and verify thumbnail from registration ticket
-#   o run nsfw detection
-#   o run dupe detection
+#   o validate activation ticket
+#     o signatures are correct
+#     o hash is correct
+#   o blockchain validations
+#     o validate that activation ticket belongs to registration ticket
+#   o reg ticket validations
+#     o regticket contains the same list of MNs
+#     o regticket has not been activated yet
+#     o regticket contains a file hash which is not registered yet
+#     o regticket contains the hash of the actual file hash
+#     o regticket contains the actual metadata hash
+#     o validate metadata
+#       o thumbnail
+#     o check fingerprint for dupes
+#     o check image for nsfw
 #   o sign
 
 
-def get_fingerprint(image_file):
-    # TODO:
-    return "DUMMY FINGERPRINT"
-
-
-def create_registration_ticket(image_file, pubkey):
+def create_registration_ticket(image_file_path, pubkey, dupedetector, combined_list):
     # 1) Artist fills registration ticket
+
+    image_file_data = open(image_file_path, "rb").read()
+
+    # read the actual image file
+    # TODO: get the fingerprint from the actual file
+    # image = keras.preprocessing.image.load_img(image_file_path, target_size=TARGET_SIZE)
+    # fingerprints = dupedetector.compute_deep_learning_features(image)
+    # combined = combine_fingerprint_vectors(fingerprints)
+    # combined_list = combined.values.tolist()[0]
+    print("FINGERPRINTS", len(combined_list))
 
     datadict = {
         "ARTIST_PUBKEY": base64.b64decode(pubkey),
@@ -59,19 +81,33 @@ def create_registration_ticket(image_file, pubkey):
         "ARTIST_WRITTEN_STATEMENT": "This is an example artwork",
         "ARTWORK_KEYWORD_SET": "example, test, artwork, firsttransaction",
         "TOTAL_COPIES": 10,
-        "IMAGE_HASH": get_sha3_512_func_bytes(image_file),
-        "IMAGE_FINGERPRINT_HASH": get_sha3_512_func_bytes(get_fingerprint(image_file)),
+        "IMAGE_HASH": get_sha3_512_func_bytes(image_file_data),
+        # "IMAGE_FINGERPRINT_HASH": get_sha3_512_func_bytes(combined_list),
+        # "IMAGE_FINGERPRINTS": combined_list,
     }
     ticket = ArtRegistrationRequest(datadict)
 
     # serialize and sign ticket
-    serialized_data = serialize_artregistration_ticket(ticket)
+    serialized_data = serialize_ticket(ticket)
     serialized_hash = get_sha3_512_func_bytes(serialized_data)
     return serialized_data, serialized_hash
 
 
-def client_sign_registration_ticket(serialized_hash, privkey, pubkey):
+def client_sign_ticket(serialized_hash, privkey, pubkey):
     return animecoin_id_write_signature_on_data_func(serialized_hash, privkey, pubkey)
+
+
+def create_verification_ticket(reg_ticket_id, pubkey):
+    datadict = {
+        "ART_REGISTRATION_TICKET_ID": reg_ticket_id,
+        "ARTIST_PUBKEY": base64.b64decode(pubkey),
+    }
+    ticket = ArtActivationTicket(datadict)
+
+    # serialize and sign ticket
+    serialized_data = serialize_ticket(ticket)
+    serialized_hash = get_sha3_512_func_bytes(serialized_data)
+    return serialized_data, serialized_hash
 
 
 # TODO: perhaps move this to into the proper validator?
@@ -81,7 +117,7 @@ def __validate_data(serialized_data, serialized_hash):
         raise ValueError("Hash does not match!")
 
     # unpack and validate data
-    ticket = ArtRegistrationRequest(load_artregistration_ticket(serialized_data))
+    ticket = ArtRegistrationRequest(load_ticket(serialized_data))
 
     # validate signature on data
     pubkey = base64.b64encode(ticket.ARTIST_PUBKEY)
@@ -89,52 +125,60 @@ def __validate_data(serialized_data, serialized_hash):
         raise ValueError("Validation failed!")
 
 
-def masternode_sign_registration_ticket(serialized_data, serialized_hash, client_signature, mn_privkey, mn_pubkey):
-    __validate_data(serialized_data, serialized_hash)
+def masternode_sign_registration_ticket(data, data_hash, mn_privkey, mn_pubkey):
+    __validate_data(data, data_hash)
 
     # all is well, sign the ticket
-    return animecoin_id_write_signature_on_data_func(serialized_hash, mn_privkey, mn_pubkey)
+    return animecoin_id_write_signature_on_data_func(data_hash, mn_privkey, mn_pubkey)
 
 
-def blockchain_verify_ticket(serialized_data, serialized_hash, signatures):
-    __validate_data(serialized_data, serialized_hash)
+def blockchain_verify_ticket(data, data_hash, signatures):
+    __validate_data(data, data_hash)
 
     for pubkey, signature in signatures:
-        if not animecoin_id_verify_signature_with_public_key_func(serialized_hash, signature, pubkey):
+        if not animecoin_id_verify_signature_with_public_key_func(data_hash, signature, pubkey):
             raise ValueError("Validation failed for signature: %s" % signature)
 
 
 if __name__ == "__main__":
-    image_file = open(sys.argv[1], "rb").read()
+    # TODO: remove this, this is only to speed up development
+    fingerprint_db = pickle.load(open("/home/synapse/tmp/animecoin/fingerprint_db_all_nns.pickle", "rb"))
+    filename, entry = fingerprint_db["2ef9cd445d39f3aa550c11bef5284253ef99cc93615cdde47fd045b48bb11a42"]
+    combined = combine_fingerprint_vectors(entry)
+    combined_list = combined.values.tolist()[0]
+
+    image_file_path = sys.argv[1]
     privkey, pubkey = animecoin_id_keypair_generation_func()
     masternodes = []
     for i in range(3):
         masternodes.append((animecoin_id_keypair_generation_func()))
 
+    # dupedetector = DupeDetector()
+    dupedetector = None
+
     ### STEP 1 ###
     # create ticket
-    serialized_data, serialized_hash = create_registration_ticket(image_file, pubkey)
-    print("Serialized data:", serialized_data)
-    print("Ticket created, len:", len(serialized_data))
+    registration_data, registration_hash = create_registration_ticket(image_file_path, pubkey, dupedetector, combined_list=combined_list)
+    # print("Serialized data:", serialized_data)
+    print("Ticket created, len:", len(registration_data))
 
     # compress ticket
     samples = list([x.encode("utf-8") for x in ARTREGISTRATION_KEYS.keys()])
-    print(samples)
-    compressiondict = zstd.train_dictionary(256, samples)
-    compressed_ticket = compress(serialized_data, compressiondict=compressiondict)
+    compressiondict = zstd.train_dictionary(8096, samples)
+    compressed_ticket = compress(registration_data, compressiondict=compressiondict)
     print("Compressed:", len(compressed_ticket))
 
     # sign ticket
-    client_signature = client_sign_registration_ticket(serialized_hash, privkey, pubkey)
+    client_signature = client_sign_ticket(registration_hash, privkey, pubkey)
 
     # have MNs validate and sign ticket
     signatures = [(pubkey, client_signature)]
     for mn_privkey, mn_pubkey in masternodes:
-        mn_signature = masternode_sign_registration_ticket(serialized_data, serialized_hash, client_signature, mn_privkey, mn_pubkey)
+        mn_signature = masternode_sign_registration_ticket(registration_data, registration_hash, mn_privkey, mn_pubkey)
         signatures.append((mn_pubkey, mn_signature))
 
     # submit to blockchain -> verify all signatures
-    blockchain_verify_ticket(serialized_data, serialized_hash, signatures)
+    blockchain_verify_ticket(registration_data, registration_hash, signatures)
     print("Ticket validated!")
 
     # now we have:
@@ -149,8 +193,13 @@ if __name__ == "__main__":
     compressed_signatures = compress(signature_data)
     print("Compressed signatures:", len(compressed_signatures))
 
-    print("Total sizes -> ticket: %s, signatures: %s" % (len(serialized_data), len(compressed_signatures)))
+    print("Total sizes -> ticket: %s, signatures: %s" % (len(registration_data), len(compressed_signatures)))
     print("Total compressed sizes -> ticket: %s, signatures: %s" % (len(compressed_ticket), len(compressed_signatures)))
 
     ### STEP 2 ###
-    pass
+    # construct verification ticket
+    registration_txid = 123456789   # TODO: fill this out properly
+    activation_data, activation_hash = create_verification_ticket(registration_txid, pubkey)
+
+    # sign ticket
+    client_signature = client_sign_ticket(activation_hash, privkey, pubkey)
--- src/storage_layer/test_animecoin_art_ticket.py
+++ src/storage_layer/test_animecoin_art_ticket.py
@@ -1,22 +1,113 @@
+import sys
 import json
-import zstd
+import base64
 
-from dht_prototype.masternode_modules.models import ArtTicket
+# from dht_prototype.masternode_modules.models import ArtTicket
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_compression import compress
+
+from dht_prototype.masternode_modules.helpers import get_digest as get_sha2_256_digest
+from dht_prototype.masternode_modules.animecoin_modules.animecoin_keys import animecoin_id_keypair_generation_func
+from dht_prototype.masternode_modules.models_new import MetaData, ImageData, RegistrationTicket, ActivationTicket
+
+
+class MockChain:
+    def __init__(self):
+        pass
+
+    def get_artist_user_id(self, pubkey):
+        raise NotImplementedError()
+
+    def get_all_fingerprints(self):
+        # TODO: get all fingerprints for registered art and stuff in mempool
+        raise NotImplementedError()
+
+    def art_hash_not_on_chain_or_mempool(self, art_hash):
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
-    ticket = ArtTicket()
-    d = ticket.to_dict()
+    # ticket = ArtTicket()
+    # d = ticket.to_dict()
+    #
+    # data = json.dumps(d)
+    # print("Original size: %s" % len(data))
+    #
+    # samples = [data.encode("utf-8")]
+    # cdict = zstd.train_dictionary(256, samples)
+    #
+    # compressed = compress(data)
+    # print("compressed without dict: %s" % len(compressed))
+    #
+    # compressed = compress(data, compressiondict=zstd.train_dictionary(256, samples))
+    # print("compressed with dict: %s" % len(compressed))
 
-    data = json.dumps(d)
-    print("Original size: %s" % len(data))
+    # parse parameters and generate stuff
+    fingerprint_file = sys.argv[1]
+    image_file = sys.argv[2]
+    privkey, pubkey = animecoin_id_keypair_generation_func()
+    fingerprint_floats = json.load(open(fingerprint_file))
 
-    samples = [data.encode("utf-8")]
-    cdict = zstd.train_dictionary(256, samples)
+    # our mockchain
+    blockchain = MockChain()
 
-    compressed = compress(data)
-    print("compressed without dict: %s" % len(compressed))
+    # create image
+    imagedata = open(image_file, "rb").read()
+    image = ImageData(dictionary={
+        "image": {"ARTWORK": imagedata},
+        "lubychunks": ImageData.generate_luby_chunks(imagedata),
+        "thumbnail": ImageData.generate_thumbnail(imagedata),
+    })
+    image.validate()
 
-    compressed = compress(data, compressiondict=zstd.train_dictionary(256, samples))
-    print("compressed with dict: %s" % len(compressed))
+    # create metadata
+    metadata = MetaData(dictionary={
+        "artistinfo": {
+            "ARTIST_USERID": blockchain.get_artist_user_id(base64.b64decode(pubkey)),
+            "ARTIST_NAME": "Example Artist",
+            "ARTIST_WEBSITE": "https://example.com",
+            "ARTIST_WRITTEN_STATEMENT": "This is an example artwork",
+        },
+        "artinfo": {
+            "ARTWORK_TITLE": "Example Artwork",
+            "ARTWORK_SERIES_NAME": "Example Series",
+            "ARTWORK_CREATION_VIDEO_YOUTUBE_URL": "https://www.youtube.com/watch?v=exampleid",
+            "ARTWORK_KEYWORD_SET": "example, test, artwork, firsttransaction",
+            "TOTAL_COPIES": 10,
+        },
+        "fingerprints": {
+            # "FINGERPRINTS": ImageData.generate_fingerprints(image)
+            # TODO: get rid of the faux fingerprint
+            "FINGERPRINTS": fingerprint_floats,
+        },
+        "lubyhashes": {
+            "LUBY_CHUNK_HASHES": image.lubychunks.get_hashes()
+        },
+        "thumbnailhash": {
+            "HASH": image.thumbnail.get_hash()
+        }
+    })
+    # TODO: this needs to be decided
+    # metadata.validate()
+
+    # tmp
+    assert(metadata == MetaData(serialized=metadata.serialize()))
+
+    # create registration ticket
+    regticket = RegistrationTicket(dictionary={
+        "imagedata_hash": {
+            "HASH": image.get_artwork_hash()
+        },
+        "metadata_hash": {
+            "HASH": metadata.get_hash()
+        },
+    })
+
+    actticket = ActivationTicket(dictionary={
+        "registration_ticket_txid": {
+            # TODO: get this from the blockchain - ticket hash is sha2 256!
+            "TXID": get_sha2_256_digest(regticket.serialize())
+        },
+        "metadata_hash": {
+            "HASH": metadata.get_hash()
+        }
+    })
