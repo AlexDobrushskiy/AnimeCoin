cfa43fdb2c647039d5cb86684d00489e160e0cc2
synapse <synapse@buffered.com>
2018-08-30 18:52:47 +0200

    Added final ticket format for registration ticket, refactored metadata into it

--- src/storage_layer/test_animecoin_art_ticket.py
+++ src/storage_layer/test_animecoin_art_ticket.py
@@ -6,7 +6,7 @@ from pprint import pprint
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_signatures import\
     animecoin_id_write_signature_on_data_func, animecoin_id_verify_signature_with_public_key_func
 from dht_prototype.masternode_modules.animecoin_modules.animecoin_keys import animecoin_id_keypair_generation_func
-from dht_prototype.masternode_modules.models_new import MetaData, ImageData, RegistrationTicket, ActivationTicket, \
+from dht_prototype.masternode_modules.models_new import ImageData, RegistrationTicket, ActivationTicket, \
     Signature, FinalRegistrationTicket
 from dht_prototype.masternode_modules.mockchain import BlockChain
 from dht_prototype.masternode_modules.masternode_ticketing import masternode_verify_registration_ticket,\
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     # parse parameters and generate stuff
     fingerprint_file = sys.argv[1]
     image_file = sys.argv[2]
+    # TODO: transition from base64 to binary keys to save space
     privkey, pubkey = animecoin_id_keypair_generation_func()
     fingerprint_floats = json.load(open(fingerprint_file))
 
@@ -46,8 +47,8 @@ if __name__ == "__main__":
     })
     image.validate()
 
-    # create metadata
-    metadata = MetaData(dictionary={
+    # create registration ticket
+    regticket = RegistrationTicket(dictionary={
         "artist_userid": blockchain.get_id_for_pubkey(pubkey),
         "artist_name": "Example Artist",
         "artist_website": "https://example.com",
@@ -62,20 +63,9 @@ if __name__ == "__main__":
         "fingerprints": image.generate_fingerprints(image),
         "lubyhashes": image.get_luby_hashes(),
         "thumbnailhash": image.get_thumbnail_hash(),
-    })
 
-    # see if we do indeed validate
-    metadata.validate(image)
-
-    # tmp
-    metadata_serialized = metadata.serialize()
-    assert(metadata == MetaData(serialized=metadata_serialized))
-
-    # create registration ticket
-    regticket = RegistrationTicket(dictionary={
         "author": pubkey,
         "imagedata_hash": image.get_artwork_hash(),
-        "metadata_hash": metadata.get_hash(),
     })
     regticket_serialized = regticket.serialize()
 
@@ -85,36 +75,36 @@ if __name__ == "__main__":
     })
 
     # make sure we validate correctly
-    signature_regticket.validate(regticket.serialize())
+    signature_regticket.validate(regticket)
 
     # ask MNs to sign
     mn_signatures = []
     for mn_pub in masternode_ordering:
         mn_priv = masternodes[mn_pub]
         data_from_mn = masternode_verify_registration_ticket(signature_regticket.serialize(), regticket_serialized,
-                                                             metadata.serialize(), blockchain, mn_pub, mn_priv)
+                                                             blockchain, mn_pub, mn_priv)
 
         # client parses signed ticket and validated signature
         mn_signature = Signature(serialized=data_from_mn)
 
         # is the data the same and the signature valid?
         assert(mn_signature.pubkey == mn_pub)
-        mn_signature.validate(regticket_serialized)
+        mn_signature.validate(regticket)
 
         # add signature to collected signatures
         mn_signatures.append(mn_signature)
 
     # create combined registration ticket
     final_regticket = FinalRegistrationTicket(dictionary={
-        "metadata": metadata_serialized,
-        "registration_ticket": regticket_serialized,
-        "signature_author": signature_regticket.serialize(),
-        "signature_1": mn_signatures[0].serialize(),
-        "signature_2": mn_signatures[1].serialize(),
-        "signature_3": mn_signatures[2].serialize(),
+        "registration_ticket": regticket.to_dict(),
+        "signature_author": signature_regticket.to_dict(),
+        "signature_1": mn_signatures[0].to_dict(),
+        "signature_2": mn_signatures[1].to_dict(),
+        "signature_3": mn_signatures[2].to_dict(),
     })
     final_regticket.validate(masternode_ordering, blockchain)
-    assert (final_regticket == FinalRegistrationTicket(serialized=final_regticket))
+    assert (final_regticket == FinalRegistrationTicket(serialized=final_regticket.serialize()))
+    pprint(final_regticket.to_dict())
     exit()
 
     # ask first MN to store regticket on chain
@@ -128,5 +118,4 @@ if __name__ == "__main__":
     actticket = ActivationTicket(dictionary={
         # TODO: get this from the blockchain - ticket hash is sha2 256!
         "registration_ticket_txid": regticket_txid,
-        "metadata_hash": metadata.get_hash(),
     })
