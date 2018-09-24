5c9b40fdc47c023cf6122b1f243506fec2919e29
synapse <synapse@buffered.com>
2018-09-24 16:38:53 +0200

    Removed old refactor pieces

--- src/python_layer/file_storage_module/helpers.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import math
-
-
-def get_sha256_hash_of_input_data_func(input_data_or_string):
-    if isinstance(input_data_or_string, str):
-        input_data_or_string = input_data_or_string.encode('utf-8')
-    sha256_hash_of_input_data = hashlib.sha3_256(input_data_or_string).hexdigest()
-    return sha256_hash_of_input_data
