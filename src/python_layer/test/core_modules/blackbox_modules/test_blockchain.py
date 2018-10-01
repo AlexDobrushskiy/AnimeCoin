0068565033c8a6c0acfbbd970c15ef0d9423d1f1
synapse <synapse@buffered.com>
2018-10-01 17:12:31 +0200

    Added integration test blockchain serialization

--- src/python_layer/test/core_modules/blackbox_modules/test_blockchain.py
+++ src/python_layer/test/core_modules/blackbox_modules/test_blockchain.py
@@ -1,21 +1,43 @@
+import time
 import unittest
 
+import bitcoinrpc
+
+
 from core_modules.blockchain import BlockChain
-
-# bitcoind cmdline:
-#   bitcoind -rpcuser=test -rpcpassword=testpw -regtest -server -addresstype=legacy
-# mine coins:
-#   bitcoin-cli -rpcuser=test -rpcpassword=testpw -regtest generate 100
-
-# TODO: this is an integration test
-# TODO: it misses the actual part that starts animecoind
+from test.helpers import Daemon
 
 
 class TestBlockChain(unittest.TestCase):
-    @unittest.skip("TODO")
+    def setUp(self):
+        self.daemon = Daemon("rt", "rt", 10001, "127.0.0.1", 10002)
+        self.daemon.start()
+
+        # connect to daemon
+        while True:
+            blockchain = BlockChain(user=self.daemon.rpcuser,
+                                    password=self.daemon.rpcpassword,
+                                    ip=self.daemon.ip,
+                                    rpcport=self.daemon.rpcport)
+            try:
+                blockchain.jsonrpc.getwalletinfo()
+            except (ConnectionRefusedError, bitcoinrpc.authproxy.JSONRPCException) as exc:
+                print("Exception %s while getting wallet info, retrying..." % exc)
+                time.sleep(0.5)
+            else:
+                break
+
+        self.blockchain = blockchain
+
+        # generate some coins
+        self.blockchain.jsonrpc.generate(100)
+        self.blockchain.jsonrpc.generate(100)
+
+    def tearDown(self):
+        self.daemon.stop()
+
     def test_blockchain_storage(self):
-        blockchain = BlockChain("rt", "rt", "127.0.0.1", 10001)
         origdata = b'THIS IS SOME TEST DATA'
-        txid = blockchain.store_data_in_utxo(origdata)
-        retdata = blockchain.retrieve_data_from_utxo(txid)
+        txid = self.blockchain.store_data_in_utxo(origdata)
+        retdata = self.blockchain.retrieve_data_from_utxo(txid)
         self.assertEquals(origdata, retdata)
---
+++ src/python_layer/test/helpers.py
@@ -0,0 +1,60 @@
+import subprocess
+import os
+import tempfile
+import shutil
+
+from core_modules.settings import NetWorkSettings
+
+
+class Daemon:
+    def __init__(self, rpcuser, rpcpassword, rpcport, ip, port):
+        self.rpcuser = rpcuser
+        self.rpcpassword = rpcpassword
+        self.rpcport = rpcport
+        self.ip = ip
+        self.port = port
+
+        self.__process = None
+        self.__test_dir = None
+
+    def start(self):
+        self.__test_dir = tempfile.mkdtemp()
+
+        animecoin_conf = os.path.join(self.__test_dir, "animecoin.conf")
+
+        # create empty animecoin.conf
+        with open(animecoin_conf, "w") as f:
+            pass
+
+        print("Started", self.__test_dir)
+
+        cmdline = [
+            NetWorkSettings.BLOCKCHAIN_BINARY,
+            "-rpcuser=%s" % self.rpcuser,
+            "-rpcpassword=%s" % self.rpcpassword,
+            "-regtest=1",
+            "-dnsseed=0",
+            # "-gen=1",
+            "-debug=1",
+            "-genproclimit=1",
+            "-equihashsolver=tromp",
+            "-showmetrics=0",
+            "-listenonion=0",
+            "-onlynet=ipv4",
+            "-txindex",
+            "-rpcport=%s" % self.rpcport,
+            "-port=%s" % self.port,
+            "-server",
+            "-addresstype=legacy",
+            "-discover=0",
+            "-datadir=%s" % self.__test_dir,
+        ]
+
+        self.__process = subprocess.Popen(cmdline, cwd=NetWorkSettings.BASEDIR)
+
+    def stop(self):
+        if self.__process is not None:
+            self.__process.terminate()
+            self.__process.wait()
+
+        shutil.rmtree(self.__test_dir)
