f8485d7741438bdfe96d298df5d7495191de91a1
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-02-22 13:11:18 +0300

    Enable local settings file for core_modules

--- src/python_layer/core_modules/settings.py
+++ src/python_layer/core_modules/settings.py
@@ -22,7 +22,7 @@ class MNDeamonSettings:
         self.basedir = ensure_type_of_field(settings, "basedir", str)
         self.ip = ensure_type_of_field(settings, "ip", str)
         self.pyrpcport = ensure_type_of_field(settings, "pyrpcport", int)
-        self.pyhttpadmin= ensure_type_of_field(settings, "pyhttpadmin", int)
+        self.pyhttpadmin = ensure_type_of_field(settings, "pyhttpadmin", int)
         self.pubkey = ensure_type_of_field(settings, "pubkey", str)
 
 
@@ -36,7 +36,6 @@ if getattr(sys, 'frozen', False):
 else:
     FROZEN = False
 
-
 NetWorkSettings = __NetworkSettings()
 
 NetWorkSettings.BASEDIR = os.path.abspath(os.path.join(__file__, "..", ".."))
@@ -47,50 +46,55 @@ NetWorkSettings.DJANGO_ROOT = os.path.join(NetWorkSettings.BASEDIR, "client_prot
 NetWorkSettings.UNSHARE_CMDLINE = ["unshare", "-mUuinpCrf", "--mount-proc=/proc", "--"]
 
 if FROZEN:
-    NetWorkSettings.DEBUG = True    # TODO: change this to False!
+    NetWorkSettings.DEBUG = True  # TODO: change this to False!
     NetWorkSettings.BLOCKCHAIN_BINARY = os.path.join(NetWorkSettings.BASEDIR, "animecoind", "animecoind")
     NetWorkSettings.DJANGOCMDLINE = [os.path.join(NetWorkSettings.BASEDIR, "start_django")]
     NetWorkSettings.IMAGEPARSERCMDLINE = NetWorkSettings.UNSHARE_CMDLINE + [
         os.path.join(NetWorkSettings.BASEDIR, "parse_image_in_jail")]
 else:
     NetWorkSettings.DEBUG = True
-    NetWorkSettings.BLOCKCHAIN_BINARY = os.path.join(NetWorkSettings.BASEDIR, "..", "..", "..", "animecoin_blockchain", "AnimeCoin", "src", "animecoind")
+    NetWorkSettings.BLOCKCHAIN_BINARY = os.path.join(NetWorkSettings.BASEDIR, "..", "..", "..", "animecoin_blockchain",
+                                                     "AnimeCoin", "src", "animecoind")
     NetWorkSettings.DJANGOCMDLINE = ["python", os.path.join(NetWorkSettings.DJANGO_ROOT, "start_django.py")]
     NetWorkSettings.IMAGEPARSERCMDLINE = NetWorkSettings.UNSHARE_CMDLINE + [
         "python", os.path.join(NetWorkSettings.BASEDIR, "parse_image_in_jail.py")]
 
 NetWorkSettings.IMAGE_PARSER_TIMEOUT_SECONDS = 3
 
-
 if NetWorkSettings.DEBUG:
     NetWorkSettings.VALIDATE_MN_SIGNATURES = False
-    NetWorkSettings.MASTERNODE_LIST = [
-        [
-            b'\x90\xf9\x17T\x1e\xf4\xc0p\xd3\xc4?b\xc8\x86Y\x93\xbeh$@\x86wO\x03\xba\xeeY-$\xa2`\x8e\x93<\xb0\xd8:\x0cF\xa3\x96,\xba4\x86y\x9c\x9aQ\xab2~\xbe\x8e\x02\xc857\x13\xf9vVIN(\x00',
-            "127.0.0.1",
-            13239
-        ],
-        [
-            b"\x9c\xa0\x90V\xaa\x0c\x10\xca\xddXUG\x92\x11\xa6M\x08w\x14\x84\xd2\xd7\xff\xc7=\x06\xc2j.\x8b\x12\xa0)L\xee\x97B\xc4\xdf\xd9\xe4\xc9\xed\x9c\xbb\x1c\xa0\x98'\xe4\r\x8el$\xea\r?\x0f\x81b\x9d\xce\xc5\xa6o\x80",
-            "127.0.0.1",
-            13240,
-        ],
-        [
-            b'\xaav\x91p\xf6_\xb3\x1a\xaa\x1f2\xf7\xbdD\x86L\'Sv\x1cV/\x8f|h\xc6=\x86a^QM\xeb\x10\x1b\xa28y,\xddP;\xe84W\xaeI\xc8\x00t\xe0k\xe0\x1a\x88\xf1%."\x88\xfd\xad\xbd?\xe3\x01',
-            "127.0.0.1",
-            13241,
-        ],
-    ]
+    try:
+        from core_modules.settings_dev import *
+
+        NetWorkSettings.MASTERNODE_LIST = MASTERNODE_LIST
+    except (ImportError, NameError):
+
+        NetWorkSettings.MASTERNODE_LIST = [
+            [
+                b'\x90\xf9\x17T\x1e\xf4\xc0p\xd3\xc4?b\xc8\x86Y\x93\xbeh$@\x86wO\x03\xba\xeeY-$\xa2`\x8e\x93<\xb0\xd8:\x0cF\xa3\x96,\xba4\x86y\x9c\x9aQ\xab2~\xbe\x8e\x02\xc857\x13\xf9vVIN(\x00',
+                "127.0.0.1",
+                13239
+            ],
+            [
+                b"\x9c\xa0\x90V\xaa\x0c\x10\xca\xddXUG\x92\x11\xa6M\x08w\x14\x84\xd2\xd7\xff\xc7=\x06\xc2j.\x8b\x12\xa0)L\xee\x97B\xc4\xdf\xd9\xe4\xc9\xed\x9c\xbb\x1c\xa0\x98'\xe4\r\x8el$\xea\r?\x0f\x81b\x9d\xce\xc5\xa6o\x80",
+                "127.0.0.1",
+                13240,
+            ],
+            [
+                b'\xaav\x91p\xf6_\xb3\x1a\xaa\x1f2\xf7\xbdD\x86L\'Sv\x1cV/\x8f|h\xc6=\x86a^QM\xeb\x10\x1b\xa28y,\xddP;\xe84W\xaeI\xc8\x00t\xe0k\xe0\x1a\x88\xf1%."\x88\xfd\xad\xbd?\xe3\x01',
+                "127.0.0.1",
+                13241,
+            ],
+        ]
 else:
     NetWorkSettings.VALIDATE_MN_SIGNATURES = True
 
 NetWorkSettings.COIN = Decimal('100000')
-NetWorkSettings.BASE_TRANSACTION_AMOUNT = Decimal('300.0') / Decimal(NetWorkSettings.COIN)  #0.00300
+NetWorkSettings.BASE_TRANSACTION_AMOUNT = Decimal('300.0') / Decimal(NetWorkSettings.COIN)  # 0.00300
 NetWorkSettings.FEEPERKB = Decimal('0.0001')
 NetWorkSettings.CDAEMON_CONFIG_FILE = "animecoin.conf"
 
-NetWorkSettings.TICKET_MATCH_EXPIRY = 30                 # 30s blocks x 30: 900s -> 15m
-
+NetWorkSettings.TICKET_MATCH_EXPIRY = 30  # 30s blocks x 30: 900s -> 15m
 
 if NetWorkSettings.DEBUG:
     NetWorkSettings.REQUIRED_CONFIRMATIONS = 1
@@ -108,25 +112,24 @@ NetWorkSettings.CNODE_HEX_DIGEST_SIZE = NetWorkSettings.CNODE_HASH_ALGO().digest
 NetWorkSettings.PYNODE_HEX_DIGEST_SIZE = NetWorkSettings.PYNODE_HASH_ALGO().digest_size * 2
 
 # TODO: set this to something more reasonable, perhaps set it per RPC call with ACLs?
-NetWorkSettings.RPC_MSG_SIZELIMIT = 100*1024*1024        # 100MB
+NetWorkSettings.RPC_MSG_SIZELIMIT = 100 * 1024 * 1024  # 100MB
 
 NetWorkSettings.REPLICATION_FACTOR = 15
-NetWorkSettings.CHUNKSIZE = 1 * 1024*1024                # 1MB
-NetWorkSettings.CHUNK_REFETCH_INTERVAL = 60              # do not retry to fetch chunk unless this many seconds elapsed
-NetWorkSettings.CHUNK_FETCH_PARALLELISM = 15             # we will fetch this many chunks simultaneously in coroutines
+NetWorkSettings.CHUNKSIZE = 1 * 1024 * 1024  # 1MB
+NetWorkSettings.CHUNK_REFETCH_INTERVAL = 60  # do not retry to fetch chunk unless this many seconds elapsed
+NetWorkSettings.CHUNK_FETCH_PARALLELISM = 15  # we will fetch this many chunks simultaneously in coroutines
 
-NetWorkSettings.MAX_TICKET_SIZE = 75 * 1024              # 75kbyte
-NetWorkSettings.IMAGE_MAX_SIZE = 100 * 1024*1024         # 100MB
-NetWorkSettings.MAX_REGISTRATION_BLOCK_DISTANCE = 3      # 3 blocks
+NetWorkSettings.MAX_TICKET_SIZE = 75 * 1024  # 75kbyte
+NetWorkSettings.IMAGE_MAX_SIZE = 100 * 1024 * 1024  # 100MB
+NetWorkSettings.MAX_REGISTRATION_BLOCK_DISTANCE = 3  # 3 blocks
 
 NetWorkSettings.THUMBNAIL_DIMENSIONS = (240, 240)
-NetWorkSettings.THUMBNAIL_MAX_SIZE = 100 * 1024          # 100 kb
+NetWorkSettings.THUMBNAIL_MAX_SIZE = 100 * 1024  # 100 kb
 
 NetWorkSettings.LUBY_REDUNDANCY_FACTOR = 10
 
 NetWorkSettings.MAX_LUBY_CHUNKS = math.ceil((NetWorkSettings.IMAGE_MAX_SIZE / NetWorkSettings.CHUNKSIZE) \
-                                  * NetWorkSettings.LUBY_REDUNDANCY_FACTOR)
-
+                                            * NetWorkSettings.LUBY_REDUNDANCY_FACTOR)
 
 if NetWorkSettings.DEBUG:
     NetWorkSettings.NSFW_THRESHOLD = 1
@@ -144,7 +147,7 @@ else:
     NetWorkSettings.DUPE_DETECTION_MODELS = ["VGG16", "Xception", "InceptionResNetV2", "DenseNet201", "InceptionV3"]
     NetWorkSettings.DUPE_DETECTION_FINGERPRINT_SIZE = 8064
 
-NetWorkSettings.DUPE_DETECTION_TARGET_SIZE = (240, 240)   # the dupe detection modules were trained with this size
+NetWorkSettings.DUPE_DETECTION_TARGET_SIZE = (240, 240)  # the dupe detection modules were trained with this size
 NetWorkSettings.DUPE_DETECTION_SPEARMAN_THRESHOLD = 0.86
 NetWorkSettings.DUPE_DETECTION_KENDALL_THRESHOLD = 0.80
 NetWorkSettings.DUPE_DETECTION_HOEFFDING_THRESHOLD = 0.48
